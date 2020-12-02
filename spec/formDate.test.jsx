47bfb645d010e7a4c1e95b9018dfeb9a946ce4a4
Mariana Mattana <mariana.mattana83@gmail.com>
2020-12-02 20:31:33 -0300

    Date tests

--- spec/formDate.test.jsx
+++ spec/formDate.test.jsx
@@ -3,6 +3,14 @@ import { mount } from 'enzyme';
 import FormDate from '../src/components/savingPlans/formDate/index';
 
 describe('date updates', () => {
+  beforeEach(() => {
+    jest
+      .spyOn(Date, 'now')
+      .mockImplementation(() => new Date('2020-12-01T11:01:58.135Z'))
+  })
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it('should increase 01 month from now', () => {
     const wrapper = mount(<FormDate />);
     wrapper.find('.right-arrow').first().simulate('click');
@@ -28,11 +36,19 @@ describe('date updates', () => {
     const wrapper = mount(<FormDate />);
     wrapper.find('.input-goal').first().simulate('keydown', { keyCode: 37 });
     expect(wrapper.state().date.format('MMMM')).toEqual("December")
-  });  
-  it('should not decrease 02 months from now, allow only future dates, using arrow keys', () => {
+  }); 
+  it('should decrease 01 month', () => {
     const wrapper = mount(<FormDate />);
+    wrapper.find('.right-arrow').first().simulate('click');
+    wrapper.find('.right-arrow').first().simulate('click');
+    wrapper.find('.left-arrow').first().simulate('click');
+    expect(wrapper.state().date.format('MMMM')).toEqual("January")
+  }); 
+  it('should decrease 01 month, using arrow keys', () => {
+    const wrapper = mount(<FormDate />);
+    wrapper.find('.input-goal').first().simulate('keydown', { keyCode: 39 });
+    wrapper.find('.input-goal').first().simulate('keydown', { keyCode: 39 });
     wrapper.find('.input-goal').first().simulate('keydown', { keyCode: 37 });
-    wrapper.find('.input-goal').first().simulate('keydown', { keyCode: 37 });
-    expect(wrapper.state().date.format('MMMM')).toEqual("December")
+    expect(wrapper.state().date.format('MMMM')).toEqual("January")
   });  
 });
